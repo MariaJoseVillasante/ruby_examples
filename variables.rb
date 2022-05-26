e7f86d09dfff72dd4d79e26e46b413874dfa320f
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-05-26 12:41:57 -0400

    Agregando ejemplos de variables en Ruby

---
+++ variables.rb
@@ -0,0 +1,14 @@
+#sumando dos variables
+x = 10
+y = 20
+suma = x + y
+puts suma
+#multiplicando variables
+precio_dolar = 20
+peso_mx = 770
+conversion = precio_dolar * peso_mx
+puts conversion
+#variables de texto
+texto_saludo="Hola mundo"
+puts texto_saludo
+puts "Hasta el próximo código " + texto_saludo
