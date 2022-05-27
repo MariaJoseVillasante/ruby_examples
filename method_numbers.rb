0c294b7cfa578192912b54b6644aebc42c1d63ca
MariaJoseVillasante <pepavillasante@yahoo.com>
2022-05-27 10:09:41 -0400

    Agregando metodosde nmeros enteros y flotantes

---
+++ method_numbers.rb
@@ -0,0 +1,20 @@
+numero_entero=1776
+#metodo para numero entero siguiente o sucesor
+puts numero_entero.next
+puts numero_siguiente=numero_entero.succ
+#metodo para validar si un numero es par o impar
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_siguiente.odd?
+puts numero_siguiente.even?
+#conversion de entero a flotante
+puts numero_siguiente/numero_entero.to_f
+puts 800/270.to_f
+#digitos de un número no****
+print 356.digits
+print 78905.digits
+#Redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
